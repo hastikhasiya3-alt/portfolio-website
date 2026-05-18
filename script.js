3ddabb9f1d1130cc7e00f11b865a03b16deee679
hastikhasiya3-alt <hastikhasiya3@gmail.com>
2026-05-18 16:34:39 +0530

    Upload portfolio website project files
    Initial upload of personal portfolio website including HTML, CSS, JavaScript, and project showcase.

---
+++ script.js
@@ -0,0 +1,40 @@
+// Typing Animation
+
+const text = [
+    "Frontend Developer",
+    "Backend Learner",
+    "AI Creator",
+    "Web Designer"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+    if(count === text.length){
+        count = 0;
+    }
+
+    currentText = text[count];
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById("typing-text").textContent = letter;
+
+    if(letter.length === currentText.length){
+
+        count++;
+        index = 0;
+
+        setTimeout(type, 1000);
+
+    }else{
+        setTimeout(type, 120);
+    }
+
+})();
+
+
+
